2014d55aefc2a6a37d48f321dd38babe1c20a208
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-04-21 13:05:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
+import React, { useState } from 'react';
 
-import React from "react";
-import './../styles/App.css';
+const FruitSearch = ({ fruits }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredFruits, setFilteredFruits] = useState([]);
 
-const App = () => {
+  const handleSearch = (event) => {
+    const { value } = event.target;
+    setSearchTerm(value);
+
+    // Filter fruits based on the search term
+    const filteredResults = fruits.filter((fruit) =>
+      fruit.toLowerCase().includes(value.toLowerCase())
+    );
+    setFilteredFruits(filteredResults);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <input
+        type="text"
+        placeholder="Search fruits..."
+        onChange={handleSearch}
+        value={searchTerm}
+      />
+      <ul >
+        {filteredFruits.map((fruit, index) => (
+          <li key={`fruit-${index}`}>{fruit}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+
+const fruitsArray =  ["apple", "banana", "cherry", "date", 'Grapes', 'Pineapple', "elderberry", 'Orange', "fig"]
+  
+export default function App() {
+  return (
+    <div>
+      <h1>Search item</h1>
+      <FruitSearch fruits={fruitsArray} />
+    </div>
+  );
+}
